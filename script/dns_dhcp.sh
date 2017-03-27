50cbe761d11f9bda4f2a8963034d848e9cec658e
Osiris <tim@chaubet.be>
2017-03-27 15:31:39 +0200

    Update dns_dhcp.sh

--- script/dns_dhcp.sh
+++ script/dns_dhcp.sh
@@ -2,12 +2,13 @@
 
 # USE the trap if you need to also do manual cleanup after the service is stopped,
 #     or need to start multiple services in the one container
-ECHO "running /script/dns_dhcp.sh"
-trap "echo TRAPed signal" HUP INT QUIT TERM
+ECHO "##### running /script/dns_dhcp.sh ######"
+#trap "echo TRAPed signal" HUP INT QUIT TERM
 
 # start service in background here
 /usr/bin/rsync --daemon --config=/etc/rsync/rsyncd.conf
 #/usr/sbin/dhcpd -d --no-pid
+/usr/sbin/sshd -D
 service isc-dhcp-server start
 service bind9 start
 
@@ -20,5 +21,6 @@ read
 service isc-dhcp-server stop
 service bind9 stop
 kill `ps -axwu | grep -e [r]sync | awk '{print $2}'` 2> /dev/null
+kill `ps -auxw | grep -e bin[\/][s][s]hd | awk '{print $2}'` 2> /dev/null
 
 echo "exited $0"
