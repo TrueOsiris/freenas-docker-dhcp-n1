f456859b36331616ce9bfc9c3418ec869a5e3538
Osiris <tim@chaubet.be>
2017-04-05 13:48:13 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,10 +10,8 @@ else
         
         chmod -R 775 /config/dhcp
         
-        #needed to fix problem with ubuntu ... and cron 
         if [ ! -f /config/dhcp/dhcpd.conf ]; then
-            cp test.txt /config/dhcp/dhcpd.conf 2>&1
-            cp /tmp/test.txt /config/dhcp/added_test.txt 2>&1
+            cp /tmp/test.txt /config/dhcp/added_test.txt
         fi
         update-locale
         date > /config/configured
