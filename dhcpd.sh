0298bdc6064ada82e02fdd217355eed5b229bb66
Osiris <tim@chaubet.be>
2017-04-05 14:50:05 +0200

    Update dhcpd.sh

--- dhcpd.sh
+++ dhcpd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec 1>>/config/dhcp/dhcp-docker.log 2>>/config/dhcp/dhcp-docker.log chpst -u root /usr/sbin/dhcpd -cf /config/dhcp/dhcpd.conf
+exec 1>>/config/dhcp/dhcp-docker.log 2>>/config/dhcp/dhcp-docker.log chpst -u root /usr/sbin/dhcpd -cf /config/dhcp/dhcpd.conf -tf /config/dhcp/dhcp-startup.log
