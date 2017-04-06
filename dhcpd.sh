1868d94c4501f557b748eefd9d98a8543cd37d2a
Osiris <tim@chaubet.be>
2017-04-06 17:32:12 +0200

    Update dhcpd.sh

--- dhcpd.sh
+++ dhcpd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec 1>/dev/null 2>/dev/null chpst -u root /usr/sbin/dhcpd -4 -cf /config/dhcp/dhcpd.conf \
+exec 1>/dev/null 2>/dev/null chpst -u root /usr/sbin/dhcpd -4 -p 67 -cf /config/dhcp/dhcpd.conf \
   -tf /config/dhcp/dhcp-startup.log -pf /config/dhcp/dhcpd.pid -lf /config/dhcp/dhcpd.leases
