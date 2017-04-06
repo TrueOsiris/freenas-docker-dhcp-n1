0d01ee0ac7eb155dbc9633a97e4c8799f89ee89f
Osiris <tim@chaubet.be>
2017-04-06 09:25:31 +0200

    Update dhcpd.sh

--- dhcpd.sh
+++ dhcpd.sh
@@ -2,12 +2,12 @@
 date >> /config/dhcp/dhcp-docker.log
 #pgrep dhcpd >>/config/dhcp/dhcp-docker.log
 if [ -f /config/dhcp/dhcpd.pid ]; then
-  if [ `cat /config/dhcp/dhcpd.pid` -eq '' ]; then
+  if [ `cat /config/dhcp/dhcpd.pid` == '' ]; then
     echo "pid file empty. Starting dhcpd." >>/config/dhcp/dhcp-docker.log
     exec 1>>/config/dhcp/dhcp-docker.log 2>>/config/dhcp/dhcp-docker.log chpst -u root /usr/sbin/dhcpd \
      -cf /config/dhcp/dhcpd.conf -tf /config/dhcp/dhcp-startup.log -pf /config/dhcp/dhcpd.pid
   else 
-    if [ ! `cat /config/dhcp/dhcpd.pid` -eq `pgrep dhcpd` ]; then
+    if [ ! `cat /config/dhcp/dhcpd.pid` == `pgrep dhcpd` ]; then
       echo "dhcpd not running. Starting." >>/config/dhcp/dhcp-docker.log
       exec 1>>/config/dhcp/dhcp-docker.log 2>>/config/dhcp/dhcp-docker.log chpst -u root /usr/sbin/dhcpd \
        -cf /config/dhcp/dhcpd.conf -tf /config/dhcp/dhcp-startup.log -pf /config/dhcp/dhcpd.pid
