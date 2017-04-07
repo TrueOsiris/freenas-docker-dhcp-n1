091c9d32b17478cf22ba1dd0a036f0af4ea34c45
Osiris <tim@chaubet.be>
2017-04-07 13:28:58 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,20 +2,31 @@
 
 set -e
 
-if [ -f /config/dhcp_setup_do_not_remove.txt ]; then
+if [ -f /config/dhcp.setup.completed ]; then
         echo 'already configured'
 else    
-        ### run once ###
-        # start sshd with root password "test"
-        echo "root:test" | chpasswd
-        sed -i 's/^PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-        /etc/init.d/ssh start
-        # set the nameservers in resolv.conf
-        echo -e "nameserver 10.10.20.20\nnameserver 10.10.30.30\nsearch chaubet\noptions ndots:0" > /etc/resolv.conf
-        #check if Directories inside of /etc/dhcp are present.
+        ### run once at container start IF no completion file ###
+        
+        # create config subfolder
         if [ ! -d /config/dhcp ]; then
            mkdir -p /config/dhcp
         fi
+        
+        # start sshd with root password "test" if not changed
+        if [ ! -f /config/dhcp/ssh.pass ]; then
+           echo "root:test" > /config/dhcp/ssh.pass
+        fi
+        cat /config/dhcp/ssh.pass | chpasswd
+        sed -i 's/^PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+        /etc/init.d/ssh start
+        
+        # set the nameservers in resolv.conf
+        if [ ! -f /config/dhcp/resolv.conf ]; then
+           echo -e "nameserver 10.10.20.20\nnameserver 10.10.30.30\nsearch chaubet\noptions ndots:0" > /config/dhcp/resolv.conf
+        fi   
+        \cp -r /config/dhcp/resolv.conf /etc/resolv.conf
+        #check if Directories inside of /etc/dhcp are present.
+
         if [ ! -f /config/dhcp/dhcpd.conf ]; then
             cp /tmp/dhcpd.conf /config/dhcp/dhcpd.conf
         fi
@@ -37,7 +48,8 @@ else
         #sleep 5s
         chmod -R 775 /config/dhcp
         update-locale
-        echo -e "do not remove this file.\nIf you do, root password will be reset to \"test\""
-        echo -e "and /etc/resolv.conf will be reset to my defaults" > /config/dhcp_setup_do_not_remove.txt
-        date >> /config/dhcp_setup_do_not_remove.txt
+        echo -e "Do not remove this file.\nIf you do, root password will be reset to the one in " \
+                "the config file ssh.pass.\n/etc/resolv.conf will be reset to the one in the config " \
+                "file.\n" > /config/dhcp.setup.completed
+        date >> /config/dhcp.setup.completed
 fi
